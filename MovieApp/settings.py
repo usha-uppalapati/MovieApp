abbe228f3596ed17870e5c49ad68f6517653c973
usha-uppalapati <usha.uppalapati@gmail.com>
2022-04-04 16:56:52 -0400

    final styling

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -130,8 +130,8 @@ MEDIA_URL = '/images/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'uppalapati.usha@gmail.com' #your email-id
-EMAIL_HOST_PASSWORD = 'Ansh18@9' #your password
+EMAIL_HOST_USER = '*****' #your email-id
+EMAIL_HOST_PASSWORD = '*****' #your password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
